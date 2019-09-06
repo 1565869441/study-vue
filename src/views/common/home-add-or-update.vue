7b18150d85cb0d818e6700b29fb998a4c9fd3a5e
cai <1565869441@qq.com>
2019-09-06 16:09:53 +0800

    提交代码

---
+++ src/views/common/home-add-or-update.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-dialog
+    title="学习记录上传"
+    :visible.sync="visible">
+    <el-upload
+      action="#"
+      list-type="picture-card"
+      limit="6"
+      :auto-upload="false">
+      <i slot="default" class="el-icon-plus"></i>
+      <div slot="file" slot-scope="{file}">
+        <img
+          class="el-upload-list__item-thumbnail"
+          :src="file.url" alt=""
+        >
+        <span class="el-upload-list__item-actions">
+        <span
+          class="el-upload-list__item-preview"
+          @click="handlePictureCardPreview(file)"
+        >
+          <i class="el-icon-zoom-in"></i>
+        </span>
+        <span
+          v-if="!disabled"
+          class="el-upload-list__item-delete"
+          @click="handleDownload(file)"
+        >
+          <i class="el-icon-download"></i>
+        </span>
+        <span
+          v-if="!disabled"
+          class="el-upload-list__item-delete"
+          @click="handleRemove(file)"
+        >
+          <i class="el-icon-delete"></i>
+        </span>
+      </span>
+      </div>
+    </el-upload>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">上传到服务器</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        visible: false,
+        fileList:[]
+      }
+    },
+    methods: {
+      init (id) {
+        this.visible = true
+      }
+    }
+  }
+</script>
